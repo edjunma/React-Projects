735b2cffb86c04821fd1fb7297eb89c5ccfbd4d5
edjunma <edjunma@gmail.com>
2020-05-31 06:13:39 -0400

    working on converter

--- currency-converter/src/App.js
+++ currency-converter/src/App.js
@@ -1,8 +1,16 @@
 import React from 'react';
 import './App.css';
+import CurrencyRow from './components/CurrencyRow';
 
 function App() {
-	return <h1>Convert</h1>;
+	return (
+		<>
+			<h1>Convert</h1>
+			<CurrencyRow />
+			<div>=</div>
+			<CurrencyRow />
+		</>
+	);
 }
 
 export default App;
