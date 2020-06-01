5cea0caf70ef27c9221974274028c96a8ca14b06
edjunma <edjunma@gmail.com>
2020-06-01 00:57:42 -0400

    add onChange to conversion for the currency

--- currency-converter/src/App.js
+++ currency-converter/src/App.js
@@ -12,6 +12,15 @@ function App() {
 	const [amount, setAmount] = useState(1);
 	const [amountInFromCurrency, setAmountInFromCurrency] = useState(true);
 
+	let toAmount, fromAmount;
+	if (amountInFromCurrency) {
+		fromAmount = amount;
+		toAmount = amount * exchangeRate;
+	} else {
+		toAmount = amount;
+		fromAmount = amount / exchangeRate;
+	}
+
 	useEffect(() => {
 		fetch(BASE_URL)
 			.then((res) => res.json())
@@ -24,6 +33,16 @@ function App() {
 			});
 	}, []);
 
+	function handleFromAmountChange(e) {
+		setAmount(e.target.value);
+		setAmountInFromCurrency(true);
+	}
+
+	function handleToAmountChange(e) {
+		setAmount(e.target.value);
+		setAmountInFromCurrency(false);
+	}
+
 	return (
 		<>
 			<h1>Convert</h1>
@@ -31,12 +50,16 @@ function App() {
 				currencyOptions={currencyOptions}
 				selectedCurrency={fromCurrency}
 				onChangeCurrency={(e) => setFromCurrency(e.target.value)}
+				onChangeAmount={handleFromAmountChange}
+				amount={fromAmount}
 			/>
 			<div className='equals'>=</div>
 			<CurrencyRow
 				currencyOptions={currencyOptions}
 				selectedCurrency={toCurrency}
 				onChangeCurrency={(e) => setFromCurrency(e.target.value)}
+				onChangeAmount={handleToAmountChange}
+				amount={toAmount}
 			/>
 		</>
 	);
--- currency-converter/src/components/CurrencyRow.js
+++ currency-converter/src/components/CurrencyRow.js
@@ -1,10 +1,10 @@
 import React from 'react';
 
 export default function CurrencyRow(props) {
-	const { currencyOptions, selectCurrency, onChangeCurrency } = props;
+	const { currencyOptions, selectCurrency, onChangeCurrency, onChangeAmount, amount } = props;
 	return (
 		<div>
-			<input type='number' className='input' />
+			<input type='number' className='input' value={amount} onChange={onChangeAmount} />
 			<select value={selectCurrency} onChange={onChangeCurrency}>
 				{currencyOptions.map((option) => (
 					<option key={option} value={option}>
