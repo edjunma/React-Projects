464289489a5af06ae92b72bc20def853d0c01c55
edjunma <edjunma@gmail.com>
2020-06-01 00:35:31 -0400

    connect currency exchange rate api

--- currency-converter/src/App.js
+++ currency-converter/src/App.js
@@ -1,13 +1,26 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import './App.css';
 import CurrencyRow from './components/CurrencyRow';
 
+const BASE_URL = 'https://api.exchangeratesapi.io/latest';
+
 function App() {
+	const [currencyOptions, setCurrencyOptions] = useState([]);
+	console.log(currencyOptions);
+
+	useEffect(() => {
+		fetch(BASE_URL)
+			.then((res) => res.json())
+			.then((data) => {
+				setCurrencyOptions([data.base, ...Object.keys(data.rates)]);
+			});
+	}, []);
+
 	return (
 		<>
 			<h1>Convert</h1>
 			<CurrencyRow />
-			<div className="equals">=</div>
+			<div className='equals'>=</div>
 			<CurrencyRow />
 		</>
 	);
