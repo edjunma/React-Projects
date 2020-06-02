f581d1c5abc1a79b5610d5221c91d5ba6e6e3cec
edjunma <edjunma@gmail.com>
2020-06-02 03:13:04 -0400

    finish image-gallery but cors server request for pixabay api error occurs

--- tailwind-image-gallery/src/App.js
+++ tailwind-image-gallery/src/App.js
@@ -1,4 +1,6 @@
 import React, { useState, useEffect } from 'react';
+import ImageCard from './components/ImageCard';
+import ImageSearch from './components/ImageSearch';
 
 function App() {
 	const [images, setImages] = useState([]);
@@ -15,39 +17,25 @@ function App() {
 				setIsLoading(false);
 			})
 			.catch((err) => console.log(err));
-	}, []);
+	}, [term]);
 
 	return (
-		<div class='max-w-sm rounded overflow-hidden shadow-lg'>
-			<img src='https://source.unsplash.com/random' alt='' className='w-full' />
-			<div className='px-6 py-4'>
-				<div className='font-bold text-purple-500 text-xl mb-2'>Photo by John Doe</div>
-				<ul>
-					<li>
-						<strong>Views: </strong>
-						4000
-					</li>
-					<li>
-						<strong>Downloads: </strong>
-						300
-					</li>
-					<li>
-						<strong>Likes: </strong>
-						400
-					</li>
-				</ul>
-			</div>
-			<div className='px-6 py-4'>
-				<span className='inline-block bg-gray-200 rounded-full px-3 y-1 text-sm font-semibold text-gray-700 mr-2'>
-					#tag1
-				</span>
-				<span className='inline-block bg-gray-200 rounded-full px-3 y-1 text-sm font-semibold text-gray-700 mr-2'>
-					#tag2
-				</span>
-				<span className='inline-block bg-gray-200 rounded-full px-3 y-1 text-sm font-semibold text-gray-700 mr-2'>
-					#tag3
-				</span>
-			</div>
+		<div className='container mx-auto'>
+			<ImageSearch searchText={(text) => setTerm(text)} />
+
+			{!isLoading && images.length === 0 && (
+				<h1 className='text-5xl text-center mx-auto mt-32'>No Images Found</h1>
+			)}
+
+			{isLoading ? (
+				<h1 className='text-6xl text-center mx-auto mt-32'>Loading...</h1>
+			) : (
+				<div className='grid grid-cols-3 gap-4'>
+					{images.map((image) => (
+						<ImageCard key={image.id} image={image} />
+					))}
+				</div>
+			)}
 		</div>
 	);
 }
