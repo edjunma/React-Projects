e57e5205193d13439451ab2a11ddb79377b2aa0f
edjunma <edjunma@gmail.com>
2020-06-03 02:09:35 -0400

    add dropdown menu function component to project

--- facebook-dropdown-menu/src/App.js
+++ facebook-dropdown-menu/src/App.js
@@ -1,4 +1,14 @@
-import React from 'react';
+import React, { useState, useEffect, useRef } from 'react';
+import { ReactComponent as BellIcon } from './icons/bell.svg';
+import { ReactComponent as MessengerIcon } from './icons/messenger.svg';
+import { ReactComponent as CaretIcon } from './icons/caret.svg';
+import { ReactComponent as PlusIcon } from './icons/plus.svg';
+import { ReactComponent as CogIcon } from './icons/cog.svg';
+import { ReactComponent as ChevronIcon } from './icons/chevron.svg';
+import { ReactComponent as ArrowIcon } from './icons/arrow.svg';
+import { ReactComponent as BoltIcon } from './icons/bolt.svg';
+
+import './index.css';
 
 function App() {
 	return (
@@ -6,9 +16,7 @@ function App() {
 			<NavItem icon={<PlusIcon />} />
 			<NavItem icon={<BellIcon />} />
 			<NavItem icon={<MessengerIcon />} />
-			<NavItem icon={<CaretIcon />}>
-				<DropdownMenu></DropdownMenu>
-			</NavItem>
+			<NavItem icon={<CaretIcon />}></NavItem>
 		</Navbar>
 	);
 }
@@ -33,6 +41,35 @@ function NavItem(props) {
 			{open && props.children}
 		</li>
 	);
+}
+
+function DropdownMenu() {
+	const [activeMenu, setActiveMenu] = useState('main');
+	const [menuHeight, setMenuHeight] = useState(null);
+	const dropdownRef = useRef(null);
+
+	useEffect(() => {
+		setMenuHeight(dropdownRef.current?.firstChild.offsetHeight);
+	}, []);
+
+	function calcHeight(el) {
+		const height = el.offsetHeight;
+		setMenuHeight(height);
+	}
+
+	function DropdownItem(props) {
+		return (
+			<a
+				href='#'
+				className='menu-item'
+				onClick={() => props.goToMenu && setActiveMenu(props.goToMenu)}
+			>
+				<span className='icon-button'>{props.leftIcon}</span>
+				{props.children}
+				<span className='icon-right'>{props.rightIcon}</span>
+			</a>
+		);
+	}
 }
 
 export default App;
